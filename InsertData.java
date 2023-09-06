9ba8b98df1b3c7c1d6593ff249d6909b5809c7a0
Anusha Malik <73341271+saymeowmeow@users.noreply.github.com>
2023-09-06 14:35:56 +0530

    Update InsertData.java

--- InsertData.java
+++ InsertData.java
@@ -10,16 +10,7 @@ public class InsertData {
     try {
       Class.forName("com.edb.Driver");
 
-            //this.con = DriverManager.getConnection("jdbc:postgresql://10.150.172.204:5444/odisha_uat", "odisha_uat", "Cdac@123");
-
-      //Prod
-     //  this.con = DriverManager.getConnection("jdbc:edb://10.150.224.20:5444/odisha_uat", "odisha_uat", "Cdac@123");
-      //"jdbc:edb://10.226.30.40:5444/odisha_uat", "odisha_uat", "Cdac@123"
-
-        // UAT
-      this.con = DriverManager.getConnection("jdbc:edb://10.10.10.54:5447/odisha_uat", "odisha_uat", "Od15hauat");
-//      this.con = DriverManager.getConnection("jdbc:edb://10.226.30.40:5444/odisha_uat", "odisha_uat", "Cdac@123");
-
+        this.con = DriverManager.getConnection("jdbc:edb://", "username", "password");
 
     } catch (Exception ex) {
       System.out.println(" error in connection  : " + ex);
