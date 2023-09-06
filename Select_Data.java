0d4e80ce92de59d55012f7773b9fb519da6ea75d
Anusha Malik <73341271+saymeowmeow@users.noreply.github.com>
2023-09-06 14:47:24 +0530

    Update Select_Data.java

--- Select_Data.java
+++ Select_Data.java
@@ -1,5 +1,4 @@
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,17 +15,7 @@ public class Select_Data {
     try {
       Class.forName("com.edb.Driver");
 
-      //con = DriverManager.getConnection("jdbc:edb://10.150.224.20:5444/odisha_uat", "odisha_uat", "Cdac@123");
-      //this.con = DriverManager.getConnection("jdbc:postgresql://10.150.172.204:5444/odisha_uat", "odisha_uat", "Cdac@123");
-
-       //Prod
-      // this.con = DriverManager.getConnection("jdbc:edb://10.150.224.20:5444/odisha_uat", "odisha_uat", "Cdac@123");
-
-        // UAT
-      this.con = DriverManager.getConnection("jdbc:edb://10.10.10.54:5447/odisha_uat", "odisha_uat", "Od15hauat");
-//      this.con = DriverManager.getConnection("jdbc:edb://10.226.30.40:5444/odisha_uat", "odisha_uat", "Cdac@123");
-
-
+      this.con = DriverManager.getConnection("jdbc:edb://", "username", "password");
 
       this.st = this.con.createStatement();
       this.rs = this.st.executeQuery(query);
